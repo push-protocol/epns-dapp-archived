818aec5497f0ed5762192b0abd381fb305b1ecf0
Harsh Rajat <ProjectHydro@3602652.no-reply.drupal.org>
2020-05-24 14:13:12 +0530

    Updates

---
+++ precache-manifest.640abe535a3fac49b755b770f4ff8cc2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cff3bde4659284b190beb7182ee74fa6",
+    "url": "https://app.epns.io/index.html"
+  },
+  {
+    "revision": "b46cef968df016df6a1a",
+    "url": "https://app.epns.io/static/css/main.d9592a6d.chunk.css"
+  },
+  {
+    "revision": "1b4bac75289211d6a609",
+    "url": "https://app.epns.io/static/js/2.40aa4237.chunk.js"
+  },
+  {
+    "revision": "9c9160c6eef74ea2cdbbf96298f1b09b",
+    "url": "https://app.epns.io/static/js/2.40aa4237.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03eb642cf1859a7c6804",
+    "url": "https://app.epns.io/static/js/3.087e51c3.chunk.js"
+  },
+  {
+    "revision": "d828d064fd611903189974745a0859f2",
+    "url": "https://app.epns.io/static/js/3.087e51c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b46cef968df016df6a1a",
+    "url": "https://app.epns.io/static/js/main.b47f1cb4.chunk.js"
+  },
+  {
+    "revision": "29c645a367da1c89b834",
+    "url": "https://app.epns.io/static/js/runtime-main.3f2c8b12.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://app.epns.io/precache-manifest.371fc3ae8e8a916166704f3a9657de35.js"
+  "https://app.epns.io/precache-manifest.640abe535a3fac49b755b770f4ff8cc2.js"
 );
 
 self.addEventListener('message', (event) => {
